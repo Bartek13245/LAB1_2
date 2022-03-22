b02c2b0cd70ad91867c0c425534f65e8113cf7be
BartekDul <101180686+Bartek13245@users.noreply.github.com>
2022-03-22 10:12:47 +0000

    Initial commit

---
+++ 1_2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main
+  {
+  public static void main(String[] args)
+  {
+     byte a=4;
+    int b=15;
+    double c=20.5;
+    char d ='a';
+     Scanner scan = new Scanner (System.in);
+    System.out.println("Jaka zmienna int: "+b);
+    
+      System.out.println("Jaka zmienna byte: "+a);
+     
+      System.out.println("Jaka zmienna char: "+d);
+   
+      System.out.println("Jaka zmienna double: "+c);
+   
+    
+  }
+}
